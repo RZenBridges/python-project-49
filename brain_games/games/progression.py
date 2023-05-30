06af7cddce19bfdce5bf32a2052e88169032ad65
RZen <bartellbe@gmail.com>
2023-05-30 15:23:14 +0000

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,7 +4,7 @@ import random
 def generate_game():
     start = random.randint(1, 15)
     step = random.randint(1, 10)
-    progression = [str(i) for i in list(range(start, start + step * 10, step))]
+    progression = [str(i) for i in range(start, start + step * 10, step)]
     hide_index = random.randint(0, 9)
     hidden_element = int(progression[hide_index])
     progression[hide_index] = '..'
