1c2db872f1c8c3e0018851db2ac82dfd72728592
RZen <bartellbe@gmail.com>
2023-08-11 10:21:12 +0000

    Refactor brain_games/games/calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,9 +4,9 @@ from operator import add, sub, mul
 
 DESCRIPTION = 'What is the result of the expression?'
 OPERATOR = {
-    '+': lambda x, y: add(x, y),
-    '*': lambda x, y: mul(x, y),
-    '-': lambda x, y: sub(y, x),
+    '+': add,
+    '*': mul,
+    '-': sub,
 }
 
 
@@ -14,5 +14,5 @@ def generate_question_and_answer():
     num1 = random.randint(1, 10)
     num2 = random.randint(11, 100)
     action = random.choice(list(OPERATOR.keys()))
-    answer = OPERATOR[action](num1, num2)
+    answer = OPERATOR[action](num2, num1)
     return f'{num2} {action} {num1}', str(answer)
